741440d1c663b77e3d8abb32c9f44c7ba3725c82
AceInSpace <chris.prodigymarketing@gmail.com>
2024-03-21 11:49:44 -0400

    Test2
    Front-end and back-end joined

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(cors())
 morgan.token('req-body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
 app.use(express.json())
+app.use(express.static('dist'))
 
 let persons = [
     { 
